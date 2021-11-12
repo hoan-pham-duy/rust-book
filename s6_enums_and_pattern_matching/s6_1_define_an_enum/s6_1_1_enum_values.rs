7cda3acd56f85983d30c11a91a64620c651c8925
Pham Duy Hoan <phamduyhoan95@gmail.com>
2021-11-13 00:44:58 +0700

    Add s6_1_define_an_enum

---
+++ s6_enums_and_pattern_matching/s6_1_define_an_enum/s6_1_1_enum_values.rs
@@ -0,0 +1,57 @@
+fn main() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("home address = {}", home.address);
+    println!("loopback address = {}", loopback.address);
+
+    /* More way
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+    */
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+            println!("You called")
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call(); 
+}
+
